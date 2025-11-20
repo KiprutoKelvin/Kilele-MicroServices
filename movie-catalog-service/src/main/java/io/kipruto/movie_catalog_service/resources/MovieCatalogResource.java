fc52a35db005b80bc5b2725d3719fce6835ebff8
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2025-11-20 16:26:03 +0300

    feat(webclient): integrate WebClient into MovieCatalogResource for movie-info lookup

--- movie-catalog-service/src/main/java/io/kipruto/movie_catalog_service/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/kipruto/movie_catalog_service/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -21,20 +22,27 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-
-        //RestClient
-        // Assume
-        // get all rated movie IDs
-        // for each movie ID call movie_info_service
         List<Rating> ratings = Arrays.asList(
                 new Rating("235456", 4),
                 new Rating("780989", 5)
         );
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+//            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+            // Using WebClient
+            Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+
             return new CatalogItem(movie.getName(), "Old times", rating.getRating());
                 })
             .collect(Collectors.toList());
